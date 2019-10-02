2d65cacc0b7644ff485f0bc0019f28c6c19680fe
Alexandre Sachs <alexandre.sachs@outlook.fr>
2019-10-02 12:38:12 +0200

    Create Tile map + update prefab player

--- RedRunner/Assets/Scripts/PlayerController.cs
+++ RedRunner/Assets/Scripts/PlayerController.cs
@@ -6,20 +6,29 @@ public class PlayerController : MonoBehaviour
 {
     #region Private Fields
 
-    private Animator animator;
-    private Rigidbody2D rigidbody;
-    private bool facingRight;
-    private Transform groundCheck;
-    private bool jump;
-    private bool grounded;
+    private Animator _animator;
+
+    private new Rigidbody2D rigidbody;
+
+    private bool _jump;
+    private bool _isGrounded;
+    private bool _facingRight;
+
+    private float _horizontalMove;
+
+    private static readonly int Jump = Animator.StringToHash("Jump");
+    private static readonly int Speed = Animator.StringToHash("Speed");
 
     #endregion
 
     #region Public Fields
 
-    public float moveForce;
-    public float maxSpeed;
-    public float jumpForce;
+    public float checkRadius = 0.5f;
+    public float speed = 5.0f;
+    public float jumpForce = 5.0f;
+
+    public LayerMask whatIsGrounded;
+    public Transform groundCheck;
 
     #endregion
 
@@ -27,72 +36,53 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        groundCheck = transform.Find("GroundCheck");
-        animator = GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Start()
     {
-        facingRight = true;
-        jump = false;
-        grounded = false;
-    }
-
-    void FixedUpdate()
-    {
-        // Cache the horizontal input.
-        float h = Input.GetAxis("Horizontal");
-
-        // The Speed animator parameter is set to the absolute value of the horizontal input.
-        animator.SetFloat("Speed", Mathf.Abs(h));
-
-        // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-        if (h * rigidbody.velocity.x < maxSpeed)
-            // ... add a force to the player.
-            rigidbody.AddForce(Vector2.right * h * moveForce);
-
-        // If the player's horizontal velocity is greater than the maxSpeed...
-        if (Mathf.Abs(rigidbody.velocity.x) > maxSpeed)
-            // ... set the player's velocity to the maxSpeed in the x axis.
-            rigidbody.velocity = new Vector2(Mathf.Sign(rigidbody.velocity.x) * maxSpeed, rigidbody.velocity.y);
-
-        //// Restrict player movement
-        //KeepWithinMinMaxRectangle();
-
-        // If the input is moving the player right and the player is facing left...
-        if (h > 0 && !facingRight)
-            // ... flip the player.
-            Flip();
-        // Otherwise if the input is moving the player left and the player is facing right...
-        else if (h < 0 && facingRight)
-            // ... flip the player.
-            Flip();
-        
-        //If the player should jump...
-        if (jump)
-        {
-            // Set the Jump animator trigger parameter.
-            animator.SetTrigger("Jump");
-
-            // Add a vertical force to the player.
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
-
-            // Make sure the player can't jump again until the jump conditions from Update are satisfied.
-            jump = false;
-        }
+        _facingRight = true;
+        _jump = false;
+        _isGrounded = false;
     }
 
     void Update()
     {
-        // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-
-        Debug.Log("Grounded ? " + grounded);
-
         // If the jump button is pressed and the player is grounded then the player should jump.
-        if (Input.GetButtonDown("Jump") && grounded)
-            jump = true;
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _jump = true;
+            rigidbody.velocity = new Vector2(_horizontalMove * speed / 2, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float jumpVelocity = _horizontalMove * speed / 2;
+        
+        // The Speed animator parameter is set to the absolute value of the horizontal input.
+        _animator.SetFloat(Speed, Mathf.Abs(_horizontalMove));
+
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGrounded);
+
+        _horizontalMove = Input.GetAxis("Horizontal");
+        // Catch the horizontal input.
+        if (_isGrounded)
+            jumpVelocity = _horizontalMove * speed;
+
+        rigidbody.velocity = new Vector2(jumpVelocity, rigidbody.velocity.y);
+
+        if (_horizontalMove > 0 && !_facingRight)
+            Flip();
+        else if (_horizontalMove < 0 && _facingRight)
+            Flip();
+
+        if (_jump)
+        {
+            _animator.SetTrigger(Jump);
+            _jump = false;
+        }
     }
 
     #endregion
@@ -101,13 +91,11 @@ public class PlayerController : MonoBehaviour
 
     void Flip()
     {
-        // Switch the way the player is labelled as facing.
-        facingRight = !facingRight;
+        _facingRight = !_facingRight;
 
-        // Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        Vector3 transformLocalScale = transform.localScale;
+        transformLocalScale.x *= -1;
+        transform.localScale = transformLocalScale;
     }
 
     #endregion
