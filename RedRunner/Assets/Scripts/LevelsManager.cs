e84c17ffbe1caa61c452895827fd4672d8ddc7ac
Charles Descoust <charles.descoust@epitech.eu>
2019-10-02 17:57:02 +0900

    LevelsManager
    Le LevelsManager est prêt à implémenter le player et les levels.

--- RedRunner/Assets/Scripts/LevelsManager.cs
+++ RedRunner/Assets/Scripts/LevelsManager.cs
@@ -1,4 +1,51 @@
-﻿public static class LevelsManager
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelsManager : MonoBehaviour
 {
-    //Dictionnary<string, int> 
+
+    #region Public Fields
+
+    public Sprite FrontDoor_Yellow;
+    public Sprite FrontDoor_Grey;
+    public Sprite FrontDoor_DarkGrey;
+    public Sprite Star;
+
+    public static LevelsManager Instance;
+
+    // Or Private ?
+    public int Life = 5;
+
+    #endregion
+
+    #region Private Fields
+
+    private Dictionary<string, int> starsCollected;
+
+    #endregion
+
+    #region Monobehaviour Callbacks
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        Instance = this;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void SetStarsCollected(string levelName, int nbStars)
+    {
+        starsCollected.Add(levelName, nbStars);
+    }
+
+    public void RemoveOneLife()
+    {
+        Life -= 1;
+    }
+
+    #endregion
 }
