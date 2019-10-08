42f9715c1ed7213395ee4ef80351854d9f4649de
Charles Descoust <charles.descoust@epitech.eu>
2019-10-08 15:55:31 +0900

    Saw Prefabs

---
+++ RedRunner/Assets/Scripts/Saw.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 euler = new Vector3(0, 0, -3);
+        transform.Rotate(euler);
+    }
+}
