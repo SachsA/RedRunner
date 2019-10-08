053acd4aaaeeceaf5adf72bec0e82e95d7193b22
Alexandre Sachs <alexandre.sachs@outlook.fr>
2019-10-08 07:05:00 +0200

    Modification prefab player : change box collider to capsule collider

---
+++ RedRunner/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+    public float cameraDistance = 120.0f;
+
+    void Awake ()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    void FixedUpdate ()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
