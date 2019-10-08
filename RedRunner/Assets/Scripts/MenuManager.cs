e1e536296f304161e0ab65089f62108f4050fea2
Charles Descoust <charles.descoust@epitech.eu>
2019-10-08 13:16:08 +0900

    Menu
    Le joueur peut modifier le volume général.

---
+++ RedRunner/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MenuCanvas;
+
+    [SerializeField]
+    private GameObject SettingsCanvas;
+
+    private bool isSettings = false;
+
+    void Start()
+    {
+        isSettings = false;
+    }
+
+    public void ChangeCanvas()
+    {
+        isSettings = !isSettings;
+        if (isSettings)
+        {
+            SettingsCanvas.SetActive(true);
+            MenuCanvas.SetActive(false);
+        } else
+        {
+            SettingsCanvas.SetActive(false);
+            MenuCanvas.SetActive(true);
+        }
+    }
+}
---
+++ RedRunner/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Slider VolumeSlider;
+
+    public AudioMixer audioMixer;
+
+    public void SetVolume()
+    {
+        audioMixer.SetFloat("Volume", VolumeSlider.value);
+    }
+}
