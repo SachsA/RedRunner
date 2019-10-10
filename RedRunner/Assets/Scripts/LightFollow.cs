0d02873a4dc26016257208dbd2b9b0e026da7a8a
Rafskoz <31534346+Rafskoz@users.noreply.github.com>
2019-10-10 08:08:41 +0100

    Add level4

---
+++ RedRunner/Assets/Scripts/LightFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    private Transform playerTransform;
+    private SpriteRenderer playerSprite;
+
+    void Awake ()
+    {
+        GetComponent<Light>();
+
+        playerTransform = player.GetComponent<Transform>();
+        playerSprite = player.GetComponent<SpriteRenderer>();
+
+    }
+
+    void FixedUpdate ()
+    {
+        if (playerSprite.enabled)
+            transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+    }
+}
